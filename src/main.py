659692192530a3c8d7d84b87effd898259b11884
ggagua <giorgi.gagua.1@btu.edu.ge>
2024-07-16 12:19:32 +0400

    Added Try/Except for handling errors

--- src/main.py
+++ src/main.py
@@ -3,16 +3,18 @@ from rdkit.Chem import Draw
 
 def substructure_search(mols, mol):
     substructure = Chem.MolFromSmiles(mol)
-
     matches = []
+    try:
+        for mol in mols:
+            molecule = Chem.MolFromSmiles(mol)
 
-    for mol in mols:
-        molecule = Chem.MolFromSmiles(mol)
+            if molecule.HasSubstructMatch(substructure):
+                matches.append(mol)
+        return matches
+    except:
+        print("Error: Invalid SMILES string")
 
-        if molecule.HasSubstructMatch(substructure):
-            matches.append(mol)
 
-    return matches
 
 # Perform a substructure search
 final = substructure_search(["CCO", "c1ccccc1", "CC(=O)O", "CC(=O)Oc1ccccc1C(=O)O"], "c1ccccc1")
