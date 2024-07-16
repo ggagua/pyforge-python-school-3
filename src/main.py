26e2ae7678191bea79e22dfcbc17d53e69820e4e
ggagua <giorgi.gagua.1@btu.edu.ge>
2024-07-16 12:41:45 +0400

    PEP 8 Cleanup, more exceptions, func description and better variable names

--- src/main.py
+++ src/main.py
@@ -1,21 +1,39 @@
 from rdkit import Chem
-from rdkit.Chem import Draw
+
 
 def substructure_search(mols, mol):
-    substructure = Chem.MolFromSmiles(mol)
-    matches = []
+    """
+      Func returns a list of all molecules from argument 1 that contain substructure from argument 2.
+
+      Arguments:
+          mols (list): A list of SMILES strings containing the molecules to search.
+          mol (str): A SMILES string which has the substructure to search for.
+
+      Returns:
+          list: A list of SMILES strings representing the molecules that contain the substructure.
+          str: An error message if an exception occurs.
+      """
     try:
-        for mol in mols:
-            molecule = Chem.MolFromSmiles(mol)
+        substructure = Chem.MolFromSmiles(mol)
+        if substructure is None:
+            raise ValueError("Unknown substructure SMILES string")
 
-            if molecule.HasSubstructMatch(substructure):
-                matches.append(mol)
+        matches = []
+        for mol_smiles in mols:
+            try:
+                molecule = Chem.MolFromSmiles(mol_smiles)
+                if molecule is None:
+                    raise ValueError(f"Invalid molecule SMILES string: {mol_smiles}")
+
+                if molecule.HasSubstructMatch(substructure):
+                    matches.append(mol_smiles)
+            except ValueError as ve:
+                print(ve)
+
         return matches
-    except:
-        return f"Error: Invalid SMILES string"
+    except Exception as e:
+        return f"Error: {str(e)}"
 
 
-
-# Perform a substructure search
-final = substructure_search(["CCO", "c1ccccc1", "CC(=O)O", "CC(=O)Oc1ccccc1C(=O)O"], "c1cccc1")
+final = substructure_search(["CCO", "c1ccccc1", "CC(=O)O", "CC(=O)Oc1ccccc1C(=O)O"], "c1ccccc1")
 print(final)
