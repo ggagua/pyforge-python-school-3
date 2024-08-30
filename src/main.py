07f4937f8619edb127b521f80fd4e4a382dc1247
ggagua <giorgi.gagua.1@btu.edu.ge>
2024-08-30 21:54:11 +0400

    fixed server balancing

--- src/main.py
+++ src/main.py
@@ -2,13 +2,12 @@ from fastapi import FastAPI
 from src.routes.molecules_router import router as molecules_router
 from src.routes.files_router import router as file_router
 from os import getenv
-
 app = FastAPI()
 
 
 @app.get("/")
 def get_server():
-    server_id = getenv("SERVER_ID", "Server balancing is turned off")
+    server_id = getenv("SERVER_ID", "Server balancing is turned off, are you running this locally?")
     return {"server_id": server_id}
 
 app.include_router(molecules_router, prefix="/molecules", tags=["Molecules"])
