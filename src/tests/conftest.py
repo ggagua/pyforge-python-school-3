730b5a962acd647baf99e8c46625376c9ac2a229
ggagua <giorgi.gagua.1@btu.edu.ge>
2024-09-14 14:41:54 +0400

    Endpoint tests added with mocking/CI adjustment

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,30 +1,10 @@
-# import pytest
-# from httpx import AsyncClient
-# from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-# from sqlalchemy.ext.asyncio import async_sessionmaker
-# from sqlalchemy.orm import sessionmaker
-# from src.main import app
-# from src.config import settings
-# from src.database import init_db, drop_test_db
-#
-# # Async engine and session setup
-# engine = create_async_engine(settings.SQLALCHEMY_DATABASE_URI, echo=True)
-# TestingSessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession)
-#
-# @pytest.fixture(scope="session", autouse=True)
-# async def setup_and_teardown():
-#     # Create test database and tables
-#     async with engine.begin() as conn:
-#         await drop_test_db(engine)
-#         await init_db()
-#     yield
-#     # Drop test database and tables after tests
-#     async with engine.begin() as conn:
-#         await drop_test_db(engine)
-#
-# @pytest.fixture
-# async def client():
-#     async with AsyncClient(
-#             transport=ASGITransport(app=app), base_url="http://test"
-#     ) as ac:
-#         yield ac
+import pytest
+from starlette.testclient import TestClient
+
+from src.main import app
+
+
+@pytest.fixture(scope="module")
+def test_app():
+    client = TestClient(app)
+    yield client
--- src/tests/test_crud.py
+++ src/tests/test_crud.py
@@ -1,10 +1,110 @@
-# import pytest
-# from src import crud, schemas
-# from sqlalchemy.ext.asyncio import AsyncSession
-#
-# @pytest.mark.asyncio
-# async def test_create_molecule(db: AsyncSession):
-#     molecule_data = schemas.MoleculeCreate(name="Test Molecule", smiles="CCO")
-#     result = await crud.create_molecule(db, molecule_data)
-#     assert result.name == molecule_data.name
-#     assert result.smiles == molecule_data.smiles
+import json
+from unittest.mock import AsyncMock, create_autospec
+from fastapi import HTTPException, status
+import pytest
+from fastapi.testclient import TestClient
+from src import crud
+from src.crud import substructure_search
+from src.schemas import MoleculeCreate, MoleculeUpdate, MoleculeSearch
+from src.main import app
+from unittest.mock import MagicMock, patch
+from rdkit import Chem
+from unittest import mock
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.engine.result import ScalarResult
+
+client = TestClient(app)
+
+
+# Decided to go with mocking database instead of connecting to it, since I've had infinite amount of problems with creating
+# test Postgres Database, migrating to it and testing it. Did not want to use sqlite as well...
+# Will improve these tests later as well.
+@pytest.fixture
+def mock_create_molecule(monkeypatch):
+    async def mock_post(db, molecule: MoleculeCreate):
+        return {"id": 1, "name": molecule.name, "smiles": molecule.smiles}
+
+    monkeypatch.setattr("src.crud.create_molecule", AsyncMock(side_effect=mock_post))
+
+
+@pytest.mark.asyncio
+async def test_add_molecule(mock_create_molecule):
+    # Mock request data
+    response = client.post("/molecules/add", json={"name": "something", "smiles": "CCCCCCHC"})
+
+    assert response.status_code == 200
+    assert response.json() == {"id": 1, "name": "something", "smiles": "CCCCCCHC"}
+
+
+@pytest.fixture
+def mock_create_molecule_conflict(monkeypatch):
+    async def mock_post(db, molecule: MoleculeCreate):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Molecule with this name or SMILES already exists."
+        )
+
+    monkeypatch.setattr("src.crud.create_molecule", AsyncMock(side_effect=mock_post))
+
+
+@pytest.mark.asyncio
+async def test_create_molecule_conflict(mock_create_molecule_conflict):
+    response = client.post("/molecules/add", json={"name": "something", "smiles": "CCCC"})
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Molecule with this name or SMILES already exists."}
+
+
+@pytest.fixture
+def mock_get_molecule(monkeypatch):
+    async def mock_get(db, molecule_id: int):
+        return {"id": molecule_id, "name": "something", "smiles": "CCCCCCHC"}
+
+    # Apply the monkeypatch for the `get_molecule` function
+    monkeypatch.setattr("src.crud.get_molecule", AsyncMock(side_effect=mock_get))
+
+
+@pytest.mark.asyncio
+async def test_get_molecule(mock_get_molecule):
+    molecule_id = 1
+    response = client.get(f"/molecules/{molecule_id}")
+
+    assert response.status_code == 200
+    assert response.json() == {"id": 1, "name": "something", "smiles": "CCCCCCHC"}
+
+
+@pytest.fixture
+def mock_update_molecule(monkeypatch):
+    async def mock_update(db, molecule_id: int, molecule: MoleculeUpdate):
+        return {"id": molecule_id, "name": molecule.name, "smiles": molecule.smiles}
+
+    monkeypatch.setattr("src.crud.update_molecule", AsyncMock(side_effect=mock_update))
+
+
+@pytest.mark.asyncio
+async def test_update_molecule(mock_update_molecule):
+    molecule_id = 1
+    updated_data = {"name": "updated_name", "smiles": "CCCC"}
+
+    response = client.put(f"/molecules/{molecule_id}", json=updated_data)
+
+    assert response.status_code == 200
+
+    assert response.json() == {"id": molecule_id, "name": "updated_name", "smiles": "CCCC"}
+
+
+@pytest.fixture
+def mock_delete_molecule(monkeypatch):
+    async def mock_delete(db, molecule_id: int):
+        return {"id": molecule_id, "name": 'updated_molecule', "smiles": 'CCCCH'}
+
+    monkeypatch.setattr("src.crud.delete_molecule", AsyncMock(side_effect=mock_delete))
+
+
+@pytest.mark.asyncio
+async def test_invalid_delete_molecule(mock_delete_molecule):
+    molecule_id = 1
+    response = client.delete(f"/molecules/{molecule_id}")
+    assert response.status_code == 200
+    assert response.json() == {"id": molecule_id, "name": "updated_molecule", "smiles": "CCCCH"}
+
+# WILL FIGURE OUT SUBSTRUCTURE SEARCH LATER, HAD SOME PROBLEMS WITH WRITING IT.
